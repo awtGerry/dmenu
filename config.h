94a5002162c89f85cbab9085004e7205c1120768
AwtGerry <monsieurgerryr@gmail.com>
2022-10-07 18:37:50 -0500

    Changed to tokyonight colors

--- config.h
+++ config.h
@@ -10,15 +10,15 @@ static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
-static char bar_color[]             = "#7b6ea4";
-static char text_white[]            = "#f9effa";
-static char text_gold[]             = "#e0cb70";
-static char selection_color[]       = "#5a78b4";
+static char bar_color[]             = "#1a1b26";
+static char text_color[]            = "#a9b1d6";
+static char text_select[]           = "#7aa2f7";
+static char selection_color[]       = "#414868";
 static const char *colors[SchemeLast][2] = {
 /*                      fg              bg          */
-	[SchemeNorm] = { text_white,    bar_color       },
-	[SchemeSel] =  { text_gold,     selection_color },
-	[SchemeOut] =  { text_white,    selection_color },
+	[SchemeNorm] = { text_color,        bar_color       },
+	[SchemeSel] =  { text_select,       selection_color },
+	[SchemeOut] =  { text_color,        selection_color },
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
